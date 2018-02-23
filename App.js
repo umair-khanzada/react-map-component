477d3f283cc14ee13c01078c99a713004a3a3312
umair-khanzada <umairkz52@gmail.com>
2018-02-23 19:50:32 +0500

    added ol support.

--- App.js
+++ App.js
@@ -5,8 +5,8 @@ class MapComponent extends React.Component{
     }
 
     render(){
-        const {data, returnCustomElement, parentClass, parentTag: ParentTag, childClass, childTag, specialClasses} = this.props,
-            ChildTag = childTag || ParentTag == 'ul' ? 'li' : ParentTag == 'tr' ? 'td' : 'div';
+        const {data, returnCustomElement, parentClass, parentTag: ParentTag, childClass, childTag, specialClasses, keys} = this.props,
+            ChildTag = childTag || (ParentTag == 'ul' || ParentTag == 'ol' ? 'li' : ParentTag == 'tr' ? 'td' : 'div');
         let first = '', middle = '', last = '', middleIndex = Math.round(data.length / 2);
 
         return (
@@ -15,12 +15,17 @@ class MapComponent extends React.Component{
                     data.map((item, index) => {
                         if(specialClasses){
                             first = index ? '' : 'map-first-item';
-                            middle = data.length > 2 && middleIndex == index ? '' : 'map-middle-item';
+                            middle = data.length > 2 && middleIndex == index ? 'map-middle-item' : '';
                             last = data.length > 1 && index == (data.length - 1) ? 'map-last-item' : '';
                         }
 
                         return returnCustomElement ?
                             returnCustomElement(item, index, {className: `${childClass} ${first}${middle}${last}`, key: index}) :
+                            keys ? keys.map((key, i) => (
+                                    <ChildTag className={`${childClass} ${first}${middle}${last}`} key={i}>
+                                        {item[key]}
+                                    </ChildTag>
+                                )) :
                             <ChildTag className={`${childClass} ${first}${middle}${last}`} key={index}>
                                 {JSON.stringify(item)}
                             </ChildTag>
@@ -35,9 +40,10 @@ class MapComponent extends React.Component{
 //map component props.
 MapComponent.propTypes = {
     data: PropTypes.array.isRequired,
+    keys: PropTypes.arrayOf(PropTypes.string),
     returnCustomElement: PropTypes.func, //call this function in return with additional third config object argument
     parentClass: PropTypes.string, //css class applied on root/parent element of map component.
-    parentTag: PropTypes.oneOf(['div', 'ul', 'tr']), //html tag for parent element;
+    parentTag: PropTypes.oneOf(['div', 'ul', 'ol', 'tr']), //html tag for parent element;
     childClass: PropTypes.string, //css class applied on each child.
     childTag: PropTypes.oneOf(['div', 'li', 'span', 'td', 'th']), //html tag for child element.
     specialClasses: PropTypes.bool, //ng-repeat like classes, Ex: $first, $middle and $last
@@ -52,25 +58,27 @@ MapComponent.defaultProps = {
 };
 
 //render root component.
-const data = [
+const exampleWithObj = [
     {name: 'Ovais', id: 1, designation: 'Frontend Developer'},
     {name: 'Zubair', id: 2, designation: 'UI Developer'},
     {name: 'Umair', id: 3, designation: 'Fullstack Developer'},
     {name: 'Sohaib', id: 4, designation: 'Game Developer'},
     {name: 'Someone else', id: 5, designation: 'Game Developer'}
 ];
+
+const exampleWithString = ["Ovais", "Zubair", "Umair", "Sohaib", "Tariq", "Someone else"];
+const exampleWithNumber = [22, 33, 44 ,55, 66, 77, 88, 99];
+
 const foo = (item, index, config) => {
     return (
-        <div>{index}</div>
+        <h1>{item}</h1>
     );
 }
 
 ReactDOM.render(
     <div>
-        <MapComponent data={data} returnCustomElement={foo} />
-        <MapComponent data={data} />
-        <MapComponent data={data} parentTag="div"/>
-        <MapComponent data={data} parentTag="tr"/>
+        <MapComponent data={exampleWithNumber} specialClasses/>
+        <MapComponent data={exampleWithObj} parentTag="tr" keys={['name', 'designation']}/>
     </div>,
     document.getElementById('app')
 )
